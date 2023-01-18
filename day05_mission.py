47b13eadf17c35b1987327ebf4fd59aabf80a25f
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-18 16:46:06 +0900

    이현서

--- day05_mission.py
+++ day05_mission.py
@@ -1,6 +1,7 @@
-#9.1
-
-def good():
-    return ['Harry', 'Ron', 'Hermione']
-
-#print(good())
+#8.12
+word = 'Got'
+a_generator = ('Got' for i in range(10))
+b_generator=(i for i in range(10))
+c_generator=a_generator,b_generator
+for k,l in c_generator:
+    print(k, l)
