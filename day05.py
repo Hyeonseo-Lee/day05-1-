0a6ac32b6af7c9833b417059cdf8d8c3fe92a3b5
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-18 15:34:22 +0900

    closures

--- day05.py
+++ day05.py
@@ -1,34 +1,18 @@
-#function
+#closures
+def calculate():
+    x = 1
+    y = 2
+    temp = 0
+    def add_sub(n):
+        nonlocal temp
+        #x = 11 # local variable
+        #temp = temp + x + n - y
+        return temp
+    return add_sub
+
+c1 = calculate()
+for i in range(5):
+    print(c1(i))
 
 
-def inha():
-    """
-    숫자 출력
-    :return:
-    """
-    print(60)
 
-
-def call_func(f):
-    """
-    매개변수로 함수를 넘겨 받아 실행
-    :param f: 매개변수가 함수
-    :return:
-    """
-    f() #넘겨 받은 함수 실행
-
-def subtract(n1, n2):
-    print(n1 -n2)
-
-
-def run_func(f, arg1, arg2):
-    f(arg1, arg2)
-
-run_func(subtract, 99, 88)
-
-
-a = (5, 7, -1)
-print(sum(a))
-
-
-call_func(inha)
