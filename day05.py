030f82307b3e52a46268dc1579d4e5557aa2df60
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-18 14:21:58 +0900

    random_2

--- day05.py
+++ day05.py
@@ -1,11 +1,12 @@
-#function
+#function return dictionary
 import random
 
-def calculate_fee(args) -> list:
+
+def calculate_fee(args) -> dict:
     """
     놀이공원 요금 계산 프로그램
     :param args: ages in list
-    :return: [전체 인원 수, 어른 수, 아이 수, 지불할 총 입장료]
+    :return: {'no_of_people':전체 인원 수, 'no_of_adult':어른 수, 'no_of_kid':아이 수, 'total_fee':지불할 총 입장료}
     """
     total = 0
     adults = 0
@@ -17,15 +18,16 @@ def calculate_fee(args) -> list:
         else:
             total = total + 3000
             kids = kids + 1
-    return [len(args), adults, kids, total]
-#vo.2
-import random
+    return {'no_of_people': len(args), 'no_of_adult': adults, 'no_of_kid': kids, 'total_fee': total}
 
 
+#print(calculate_fee.__doc__)
+help(calculate_fee)
+help(len)
 no_of_visitor = int(input('몇 분 이세요? '))
 ages = [random.randint(1, 60) for age in range(no_of_visitor)]
 results = calculate_fee(ages)
-print(f'{results[0]}명 방문 하셨고 어른 {results[1]}명, 아이 {results[2]}명 총 요금은 { results[-1]}원 입니다'
+print(f"{results['no_of_people']}명 방문 하셨고 어른 {results['no_of_adult']}명, 아이 {results['no_of_kid']}명 총 요금은 { results['total_fee']}원 입니다")
 
 
 
