4ae4b1d9c1bbe125e55b4b1850aada9afbdb1c05
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-18 14:28:06 +0900

    function2

--- day05.py
+++ day05.py
@@ -1,45 +1,20 @@
-#function return dictionary
-import random
+#function
 
 
-def calculate_fee(args) -> dict:
+def inha():
     """
-    놀이공원 요금 계산 프로그램
-    :param args: ages in list
-    :return: {'no_of_people':전체 인원 수, 'no_of_adult':어른 수, 'no_of_kid':아이 수, 'total_fee':지불할 총 입장료}
+    숫자 출력
+    :return:
     """
-    total = 0
-    adults = 0
-    kids = 0
-    for age in args:
-        if 19 <= age:  # adult
-            total = total + 10000
-            adults = adults + 1
-        else:
-            total = total + 3000
-            kids = kids + 1
-    return {'no_of_people': len(args), 'no_of_adult': adults, 'no_of_kid': kids, 'total_fee': total}
+    print(60)
 
 
-#print(calculate_fee.__doc__)
-help(calculate_fee)
-help(len)
-no_of_visitor = int(input('몇 분 이세요? '))
-ages = [random.randint(1, 60) for age in range(no_of_visitor)]
-results = calculate_fee(ages)
-print(f"{results['no_of_people']}명 방문 하셨고 어른 {results['no_of_adult']}명, 아이 {results['no_of_kid']}명 총 요금은 { results['total_fee']}원 입니다")
+def call_func(f):
+    """
+    매개변수로 함수를 넘겨 받아 실행
+    :param f: 매개변수가 함수
+    :return:
+    """
+    f() #넘겨 받은 함수 실행
 
-
-
-# def do_nothing():
-#     pass
-#
-# mamamoo = ['화사', '솔라', '휘인', '문별']
-#
-# #print(mamamoo.pop()) #삭제할 값 리턴 후 삭제
-# print(mamamoo.remove('문별')) #삭제만 함. 따라서 print함수 None 출력
-# print(mamamoo)
-#
-# do_nothing()
-# print(do_nothing())
-
+call_func(inha)
